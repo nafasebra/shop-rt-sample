eb6e305e290214c408defe77b8350fcabd85be70
Nafas Ebrahimi <ebranafas@gmail.com>
2024-06-07 18:30:04 +0330

    refactor counter component

--- src/components/shared/Counter.tsx
+++ src/components/shared/Counter.tsx
@@ -1,27 +1,55 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { MinusIcon, PlusIcon, TrashIcon } from "@heroicons/react/16/solid";
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(1);
 
-  const handleCounter = (type: "inc" | "dec") => () => {
-    if(type === "inc") {
-      setCounter(prev => {
-        return prev + 1
-      })
-      } else {
-        setCounter(prev => {
-          return prev - 1
-        })
+  const handleCounter = (type: "inc" | "dec" | "del") => () => {
+    switch (type) {
+      case "inc":
+        setCounter((prev) => {
+          return prev + 1;
+        });
+        break;
+      case "dec":
+        setCounter((prev) => {
+          return prev - 1;
+        });
+        break;
+      case "del":
+        console.log("delete");
+        break;
+      default:
+        return;
     }
-  }
+  };
 
   return (
-    <div className='flex items-center gap-3'>
-      <button className='flex items-center justify-center w-6 h-6 bg-slate-800 text-white font-bold' onClick={handleCounter('dec')}>-</button>
-      <p className='w-20 text-center'>{counter}</p>
-      <button className='flex items-center justify-center w-6 h-6 bg-slate-800 text-white font-bold' onClick={handleCounter('inc')}>+</button>
+    <div className="flex items-center gap-3">
+      {counter === 1 ? (
+        <button
+          className="flex items-center justify-center w-8 h-8 rounded-lg text-red-600 font-bold"
+          onClick={handleCounter("del")}
+        >
+          <TrashIcon className="h-6 w-6" />
+        </button>
+      ) : (
+        <button
+          className="flex items-center justify-center w-8 h-8 rounded-lg bg-slate-800 text-white font-bold"
+          onClick={handleCounter("dec")}
+        >
+          <MinusIcon className="h-6 w-6" />
+        </button>
+      )}
+      <p className="w-24 text-center font-bold">{counter}</p>
+      <button
+        className="flex items-center justify-center w-8 h-8 rounded-lg bg-slate-800 text-white font-bold"
+        onClick={handleCounter("inc")}
+      >
+        <PlusIcon className="h-6 w-6" />
+      </button>
     </div>
-  )
+  );
 }
 
-export default Counter
+export default Counter;

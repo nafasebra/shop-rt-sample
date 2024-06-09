b03fc2dff61f91a0b53f178c48abb2a2252b311e
Nafas Ebrahimi <ebranafas@gmail.com>
2024-06-09 12:06:07 +0330

    feat: add proce calculation on shopping cart

--- src/components/pages/ShoppingCart/CHeckout.tsx
+++ src/components/pages/ShoppingCart/CHeckout.tsx
@@ -1,17 +1,38 @@
+import { useAppSelector } from "../../../service/store";
+import Products from "../../../assets/product.json";
+import { ProductDataType } from "../Homepage/type";
+
+const productsMap: Record<string, ProductDataType> = Products.reduce((map, product) => {
+  map[product.id] = product;
+  return map;
+}, {} as Record<string, ProductDataType>);
 
 function CHeckout() {
+  const state = useAppSelector((state) => state.cart);
+  const price = state.carts.length
+    ? state.carts.reduce((prev, curr) => {
+        const product = productsMap[curr.id];
+        return product ? prev + (product.price * curr.count) : prev;
+      }, 0)
+    : 0;
+
   return (
-    <div className='space-y-6 w-full lg:w-[40%] rounded-lg border border-gray-300 p-6'>
+    <div className="space-y-6 w-full lg:w-[40%] rounded-lg border border-gray-300 p-6">
       <p className="font-bold text-xl">Checkout</p>
-      <div className='flex flex-col gap-4'>
-        <div className='flex items-center justify-between'>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between">
           <p>price</p>
-          <p className='font-bold text-xl'>$12</p>
+          <p className="font-bold text-xl">${price}</p>
         </div>
-        <button type='button' className='py-3 px-6 text-white bg-slate-800 rounded-lg text-lg font-bold'>Pay</button>
+        <button
+          type="button"
+          className="py-3 px-6 text-white bg-slate-800 rounded-lg text-lg font-bold"
+        >
+          Pay
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default CHeckout
+export default CHeckout;
